3b39c10c117ae28a031c2b033417f5589e294400
Sakshi <sakshi.ahlawat@stackroute.in>
2023-06-30 10:23:49 +0530

    springDocs added

--- src/main/java/com/stackroute/telemedicineconsultation/authentication/JwtConfig/SecurityTokenGeneratorImpl.java
+++ src/main/java/com/stackroute/telemedicineconsultation/authentication/JwtConfig/SecurityTokenGeneratorImpl.java
@@ -12,23 +12,48 @@ import java.util.Map;
 @Service
 public class SecurityTokenGeneratorImpl implements SecurityTokenGenerator {
 
-    @Value("${jwt.secretKey}")
-    private String secretKey;
+    // @Value("${jwt.secretKey}")
+    // private String secretKey;
 
-    @Override
-    public Map<String, String> generateToken(UserModel user) {
-        Map<String, String> map = new HashMap<>();
-        long currentTimeInMilli = System.currentTimeMillis();
-        String jwtToken = Jwts.builder()
-                .setSubject(user.getEmailId())
-                .setIssuedAt(new Date(currentTimeInMilli))
-                .signWith(SignatureAlgorithm.HS512, secretKey)
-                .compact();
-        map.put("token", jwtToken);
-        map.put("message", "User successfully logged in");
-        map.put("emailId", user.getEmailId());
-        return map;
-    }
+    // @Override
+    // public Map<String, String> generateToken(UserModel user) {
+    //     Map<String, String> map = new HashMap<>();
+    //     long currentTimeInMilli = System.currentTimeMillis();
+    //     String jwtToken = Jwts.builder()
+    //             .setSubject(user.getEmailId())
+    //             .setIssuedAt(new Date(currentTimeInMilli))
+    //             .setExpiration(new Date(currentTimeInMilli + 600000))
+    //             .signWith(SignatureAlgorithm.HS512, secretKey)
+    //             .compact();
+    //     map.put("token", jwtToken);
+    //     map.put("message", "User successfully logged in");
+    //     map.put("emailId", user.getEmailId());
+    //     return map;
+    // }
+
+    private final String secretKey;
+
+public SecurityTokenGeneratorImpl(@Value("${jwt.secretKey}") String secretKey) {
+    this.secretKey = secretKey;
+}
+
+@Override
+public Map<String, String> generateToken(UserModel user) {
+    long currentTimeInMilli = System.currentTimeMillis();
+    String jwtToken = Jwts.builder()
+            .setSubject(user.getEmailId())
+            .setIssuedAt(new Date(currentTimeInMilli))
+            .setExpiration(new Date(currentTimeInMilli + 600000))
+            .signWith(SignatureAlgorithm.HS512, secretKey)
+            .compact();
+
+    Map<String, String> map = new HashMap<>();
+    map.put("token", jwtToken);
+    map.put("message", "User successfully logged in");
+    map.put("emailId", user.getEmailId());
+
+    return map;
+}
 
 
 
